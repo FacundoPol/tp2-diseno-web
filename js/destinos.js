e91530628644821df3ab4afe8bd52e8c6eebc1ff
MiguelAbregu <miguel_af_555@hotmail.com>
2026-04-25 17:40:38 -0300

    migracion a boostrap y simplificacion de filtros jquery

--- js/destinos.js
+++ js/destinos.js
@@ -1,4 +1,6 @@
-const observer = new IntersectionObserver((entries) => {
+$(document).ready(function() {
+
+    const observer = new IntersectionObserver((entries) => {
         entries.forEach(entry => {
             if (entry.isIntersecting) {
                 entry.target.classList.add('visible');
@@ -7,3 +9,24 @@ const observer = new IntersectionObserver((entries) => {
     }, { threshold: 0.1 });
 
     document.querySelectorAll('.reveal').forEach(el => observer.observe(el));
+
+    //  Sistema de Filtros 
+    $('input[type="radio"]').on('change', function() {
+        var filtro = $(this).attr('id'); 
+        var articulos = $('.row .news-item'); 
+
+        if (filtro === 'todas') {
+            articulos.show(); // Muestra todos
+        } else {
+            articulos.hide(); // Esconde todos primero
+            articulos.filter('.' + filtro).show(); // Filtra y muestra los que coinciden
+        }
+    });
+
+    //  Efecto Zoom 
+    $('.news-item').hover(
+        function() { $(this).addClass('card-zoom-activo'); }, 
+        function() { $(this).removeClass('card-zoom-activo'); }
+    );
+
+});
