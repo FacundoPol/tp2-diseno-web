ced362dd41b77ec2cf9bc4be4232b8978cfa1fd9
Facundo Pol <facundopol24@gmail.com>
2026-04-26 18:37:17 -0300

    Cambio de cards y rating e implementacion de jquery en agencias - FacundoPol

---
+++ js/agencias.js
@@ -0,0 +1,28 @@
+$(function()
+{
+    $(".card").on("click",function(){
+        $(this).toggleClass("girar");
+    });
+});
+
+$(function(){
+
+    $(".rating .star").on("click", function(){
+
+        let valor = $(this).data("value");
+
+        let contenedor = $(this).parent();
+
+        contenedor.find(".star").removeClass("active");
+
+        contenedor.find(".star").each(function(){
+
+            if($(this).data("value") <= valor){
+                $(this).addClass("active");
+            }
+
+        });
+
+    });
+
+});
